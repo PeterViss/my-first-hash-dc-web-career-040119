182c890ce041d744e3ad47d09996688096d6a710
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-05 23:11:18 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-cool_beans = {"pinto", "black", "re-fried"}
+cool_beans = {"pinto" => 3 , "black" => 5, "re-fried" => 7}
 
 end
 
